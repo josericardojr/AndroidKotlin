0c4e587fd7c766e81163b8ff9033e184e157e30d
Jose Ricardo <josericardo.jr@gmail.com>
2024-07-05 10:33:09 -0300

    Jokenpo game layout completed

--- AS/Jokenpo/app/src/main/java/com/tecnologiaemjogo/jokenpo/MainActivity.kt
+++ AS/Jokenpo/app/src/main/java/com/tecnologiaemjogo/jokenpo/MainActivity.kt
@@ -15,32 +15,7 @@ import com.tecnologiaemjogo.jokenpo.ui.theme.JokenpoTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            JokenpoTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+        setContentView(R.layout.main_activity)
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    JokenpoTheme {
-        Greeting("Android")
-    }
-}
